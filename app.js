8ad1326c0c9322b6f29a2dbfa328f5ab464072fc
Ronfel <gabrielronfel@gmail.com>
2024-09-07 13:46:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log(dados);
+    let section = document.getElementById("resultados-pesquisa")
+    console.log(section)
+
+
+    section.innerHTML = 
+    `<div class="item-resultado">
+                <h2><a href="#" target="_blank">${dados[0].titulo}</a></h2>
+                <p class= RenanPlays, conhecido como a Caveira Gamer da Calvosfera, é um YouTuber que conquistou seu espaço na plataforma com seu humor ácido e gameplay irreverente. Com um canal repleto de gameplays de diversos jogos, Renan se destaca por suas reações exageradas, piadas sobre calvície e um senso de humor peculiar que cativou um público fiel.${dados[1].descricao}</p>
+                <a href="https://desciclopedia.org/wiki/REnanPLAY" target="_blank">Mais info...</a>
+              </div>`
---
+++ dados.js
@@ -0,0 +1,16 @@
+        let Renanplays = {
+        titulo : "Renanplays",
+        discricao : "RenanPlays, conhecido como a Caveira Gamer da Calvosfera, é um YouTuber que conquistou seu espaço na plataforma com seu humor ácido e gameplay irreverente. Com um canal repleto de gameplays de diversos jogos, Renan se destaca por suas reações exageradas, piadas sobre calvície e um senso de humor peculiar que cativou um público fiel.",
+        link : "https://desciclopedia.org/wiki/REnanPLAY"
+        }
+        let Ronaldinho = {
+        titulo: "Ronaldinho ",
+        descricao: "Ronaldinho, um dos pilares da Calvosfera com seu famoso FALARO, é conhecido por seu humor inteligente e suas análises profundas sobre jogos e cultura pop. Com um estilo mais reflexivo, ele conquistou um público que busca conteúdo de qualidade e bem elaborado.", // Substitua por uma descrição detalhada
+        link: "https://link_para_o_perfil_do_Ronaldinho" 
+    };
+    let LuanGameplays = {
+        titulo: "LuanGameplays",
+        descricao: "Luangameplays é um dos streamers mais carismáticos da Calvosfera, conhecido por suas lives animadas e interações com a comunidade. Com um estilo mais descontraído, ele conquistou um público que busca diversão e entretenimento.", 
+        link: "https://link_para_o_perfil_do_LuanGameplays" 
+    }
+    
